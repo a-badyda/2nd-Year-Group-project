a3cfa7391e898307a73c925a2516556f42553381
Samuel Jackson <slj11@aber.ac.uk>
2012-10-30 15:51:57 +0000

    Started linking some JavaScript to the login page.

---
+++ N17/src/JavaScript/login.js
@@ -0,0 +1,14 @@
+/*
+Login.js
+----------------
+Handle input from the login form
+----------------
+Author:	Samuel Jackson
+Email:	slj11@aber.ac.uk	
+*/
+
+function loginInit() {
+	$("#submit").click(function() {
+		return false;
+	});
+}
