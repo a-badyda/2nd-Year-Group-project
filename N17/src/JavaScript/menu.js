fa077602335f794aac4487f48fe7ca859d3fd99e
Samuel Jackson <slj11@aber.ac.uk>
2012-11-08 12:53:32 +0000

    Added a JavaScript file ready to process the menu.

---
+++ N17/src/JavaScript/menu.js
@@ -0,0 +1,17 @@
+/*
+Menu.js
+----------------
+JavaScript to control the menu.
+----------------
+Author:	Samuel Jackson
+Email:	slj11@aber.ac.uk	
+*/
+
+$(document).ready(function() {
+
+	function getPage() {
+		return location.pathname.substring(1);
+	}
+});
+
+
