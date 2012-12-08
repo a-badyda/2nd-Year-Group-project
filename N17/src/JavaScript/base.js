04539bb92b247a819c563cf03188c0cfb01fbe21
Samuel Jackson <slj11@aber.ac.uk>
2012-12-08 11:48:26 +0000

    Adding base.js to the git repo.

---
+++ N17/src/JavaScript/base.js
@@ -0,0 +1,31 @@
+/*
+Base.js
+----------------
+JavaScript for loading HTML data into the base.html page.
+Should grab the requested HTML file and output it here.
+----------------
+Author:	Samuel Jackson
+Email:	slj11@aber.ac.uk
+*/
+
+$(document).ready(function () {	
+	var $_GET = {};
+	
+	document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+		function decode(s) {
+			return decodeURIComponent(s.split("+").join(" "));
+		}
+
+		$_GET[decode(arguments[1])] = decode(arguments[2]);
+	});
+
+	var page = $_GET["page"];
+	if (page == '') {
+		window.location.replace("login.html");
+	} else {
+		$.get(page + '.html', function (response) {
+			$('body').append(response);
+		});
+	}
+});
+
