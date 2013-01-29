8044e7650c3a4096fd73f59454e1ee5345775a60
Silhab Csoma <csomas.0gmail.com>
2013-01-29 15:43:01 +0000

    JUnit for Request and Breeding

---
+++ N17/src/JUnit/BreedingTest.java
@@ -0,0 +1,28 @@
+package JUnit;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import root.Breeding;
+import root.Monster;
+import root.User;
+
+public class BreedingTest {
+
+	Breeding br = new Breeding();
+	
+	@Test
+	public void testBreed() {
+		User u1 = new User("A", "Code");
+		Monster m1 = new Monster(1,"Pok");
+		User u2 = new User("B", "Edoc");
+		Monster m2 = new Monster(2, "Kop");
+		ArrayList<String> children = br.Breed(u1, u2, m1, m2);
+		System.out.println(children);
+		assertNotNull(children);
+	}
+
+}
